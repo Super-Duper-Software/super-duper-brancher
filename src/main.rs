0daaeb2e4898cd971c0ea68adce9f323afea9550
Stephen Sparling <sparlingcreations@gmail.com>
2026-09-06 17:27:28 -0400

    run git command, print back response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let output = Command::new("git")
+        .args(["rev-parse", "--abbrev-ref", "HEAD"])
+        .output()
+        .expect("Failed to execute process");
+    let output_string = String::from_utf8(output.stdout)
+        .expect("Failed to parse command output");
+    println!("response: {}", output_string.trim());
 }
