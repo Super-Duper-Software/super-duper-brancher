34d07c34e2f1d7ebee906a7fd6a8a0b11847e494
Stephen Sparling <sparlingcreations@gmail.com>
2026-09-07 11:37:29 -0400

    remove unnecessary post merge script

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,6 @@ enum Commands {
     Init,
     Status,
     HookPostCheckout(HookPostCheckoutArgs),
-    HookPostMerge(HookPostMergeArgs),
 }
 
 #[derive(Args)]
@@ -69,13 +68,6 @@ struct HookPostCheckoutArgs {
     ref_new_head: String,
     /// 0 for file checkout, 1 for branch checkout
     is_branch_checkout: i32,
-}
-
-#[derive(Args)]
-
-struct HookPostMergeArgs {
-    /// 0 for standard, 1 for squash
-    is_squash: i32,
 }
 
 fn get_script(subcommand: &str) -> String {
@@ -109,12 +101,10 @@ fn get_epoch_secs() -> u64 {
 
 fn init() {
     let post_checkout_script = get_script("hook-post-checkout");
-    let post_merge_script = get_script("hook-post-merge");
 
     // TODO: check if exists, if does, need to surface error to user
     // and maybe allow for a --force flag that will overwrite
     write_and_set_perms(".git/hooks/post-checkout", &post_checkout_script);
-    write_and_set_perms(".git/hooks/post-merge", &post_merge_script);
 
     let mut state = read_state().expect("Could not read state");
     state.installed_at = get_epoch_secs();
@@ -236,13 +226,6 @@ fn hook_post_checkout(_prev: &str, _new: &str, is_branch_checkout: &i32) {
     write_state(&state);
 }
 
-fn hook_post_merge(is_squash: &i32) {
-    println!(
-        "You invoked the hook_post_merge command. is squash {}",
-        is_squash
-    );
-}
-
 fn main() {
     let cli = Cli::parse();
 
@@ -255,9 +238,6 @@ fn main() {
             is_branch_checkout,
         }) => {
             hook_post_checkout(ref_prev_head, ref_new_head, is_branch_checkout);
-        }
-        Commands::HookPostMerge(HookPostMergeArgs { is_squash }) => {
-            hook_post_merge(is_squash);
         }
     }
 }
